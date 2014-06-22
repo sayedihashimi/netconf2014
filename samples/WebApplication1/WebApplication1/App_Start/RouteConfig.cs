b6ecdb0045fb508d99d8225b457f273414dd84c7
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-06-22 11:36:53 -0700

    slight change

--- samples/WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ samples/WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -11,7 +11,7 @@ namespace WebApplication1 {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "Default2",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
